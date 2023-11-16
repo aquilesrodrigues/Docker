8816053464105052b77e4522ec889349dd110e65
Aquiles Rodrigues <aquilesrodrigues@outlook.com.br>
2023-11-16 13:18:30 -0300

    alter folder file multiple db

--- docker-compose/suporte-chur/environment/postgresql/conf.d/multiple-db.sh
+++ docker-compose/suporte-chur/environment/postgresql/conf.d/multiple-db.sh
@@ -5,7 +5,7 @@ set -u
 
 function create_user_and_database() {
 	local database=$1
-	echo "  Creating user and database '$database'"
+	echo "  Criando usuário e Banco de dados '$database'"
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
 	    CREATE USER $database;
 	    CREATE DATABASE $database;
@@ -14,9 +14,9 @@ EOSQL
 }
 
 if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
-	echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
+	echo "Criação de vários bancos de dados solicitados: $POSTGRES_MULTIPLE_DATABASES"
 	for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
 		create_user_and_database $db
 	done
-	echo "Multiple databases created"
+	echo " Multiplos Bancos creados! "
 fi

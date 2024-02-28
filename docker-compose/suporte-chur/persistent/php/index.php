e3b452d9f535866026ef9f45dfd4df2785acb5a6
Aquiles Rodrigues <aquilesrodrigues@outlook.com.br>
2024-02-28 16:24:32 -0300

    modify index.php for connect db

--- docker-compose/suporte-chur/persistent/php/index.php
+++ docker-compose/suporte-chur/persistent/php/index.php
@@ -1,28 +1,24 @@
 <?php
 
-echo "<h1>APP1</h1>";
-echo "PHP rodando...";
-echo "<br>";
-echo "<br>";
-
+echo "<h1>Informações sobre o PHP</h1>";
+echo "<h3> Testando os serviços do PHP </h3>";
 if (imagetypes() & IMG_PNG) {
-    echo "O suporte a PNG está ativado \n";
-    echo "\n";
+    echo "1 - O suporte a PNG está ativado";
+    echo "<br>";
 }
 
+echo "<h3> Chamando o PHP Info </h3>";
 
 phpinfo();
 
+echo "<h3> Testando conexões a Banco de Dados </h3>";
 
-/*
-
-echo "Teste do Banco de Dados MariaDB";
-
+echo "<H4> 1 - MariaDB </H4>";
 // Configurações de acesso
-$servername = "mariadb_compose";
-$username = "root";
-$password = "db@2024";
-$database = "db_mysql_exemplo";
+$servername = "172.18.0.1";
+$username = "andre.gustavo";
+$password = "!A871902#";
+$database = "";
 $port = "3306";
 
 // Criar conexão
@@ -30,12 +26,15 @@ $conn = new mysqli($servername, $username, $password, $database, $port);
 
 // Checa conexão
 if ($conn->connect_error) {
-  die("Erro de conexão ao MySQL: " . $conn->connect_error);
+  die("<br> Erro de conexão ao MariaDB:<br>" . $conn->connect_error);
+} else {
+    echo "<br>";
+    echo "Conexão com MariaDB OK!";
+    echo "<br>";
 }
-echo "<br>";
-echo "Conexão com MySQL OK!";
-echo "<br>";
-echo "<br>";
+
+
+/*
 
 echo "Teste do Banco de Dados Postgres";
 
@@ -70,5 +69,4 @@ if ($redis->ping()) {
     die('Erro de conexão ao Redis');
 }
 */
-
 ?>
